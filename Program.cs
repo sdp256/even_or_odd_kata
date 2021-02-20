0e65845f1520f66d56473791204c205d82b0082a
sdp256 <steevnpairy@gmail.com>
2021-02-20 12:33:30 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace even_or_odd_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            EvenOrOdd(number);
+           
+            
+
+
+            ///number % 2 = 0 write even, else write odd
+        }
+        static string EvenOrOdd(int x)
+        {
+            
+            if (x % 2 == 0)
+            {
+                return ("Even");
+            }
+            else
+            {
+                return ("Odd");
+            }
+           
+        }
+
+
+
+    }
+}
